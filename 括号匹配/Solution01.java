a0509e14eb475c1a5d4ff9c83cde6f19c7684911
Calos <fxiaoyu97@qq.com>
2020-12-23 11:24:07 +0800

    Create Solution01.java
    使用栈解法，但是此处有一个遗留问题

---
+++ 括号匹配/Solution01.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * 使用栈解法，此处有一个遗留问题
+ * 如果在出栈的时候，栈为空会抛异常，缺少判断
+ */
+public class Solution01 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        int count = 0;
+        int len = str.length() - 1;
+        Stack stack = new Stack<String>();
+        while (len >= 0) {
+            char c = str.charAt(len);
+            if (c == ')') {
+                stack.push(c);
+            } else if (c == '(') {
+                stack.pop();
+                ++count;
+            }
+            --len;
+        }
+        if (stack.isEmpty()) {
+            System.out.println(count);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
